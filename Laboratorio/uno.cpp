a7ac3689e1410cd005a50e4216809939bb2a41fd
HubDevCode <128942387+HubDevCode@users.noreply.github.com>
2025-10-07 22:08:23 +0200

    Create uno.cpp
    Creazione del fil per la somma di due numeri

---
+++ Laboratorio/uno.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    int a, b;
+    
+    
+    cout << "Inserisci primo numero: ";
+    cin >> a;
+    cout << "Inserisci secondo numero: ";
+    cin >> b;
+    
+    //somma
+    int somma = a+b;
+    
+    cout << "La somma è: " << somma << endl;
+
+    return 0;
+}
